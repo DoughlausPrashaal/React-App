e512150c26940e9e742304655a11bd23d388170e
DoughlausPrashaal <doughlausprashaal100@gmail.com>
2024-04-05 09:22:20 +0530

    React APP

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Greet/>
+    </>
+  )
+
+}
+
+function Greet(){
+  return (
+    <div>
+      <h1>
+        Hello world 
+      </h1>
+    </div>
+  )
+}
+
+export default App
